d644eb1baeaac9ff00b9d2552b9f6ca9f28a8384
Your Name taro <horn43inoue@gmail.com>
2022-07-25 22:51:36 +0900

    changes for v3

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Authenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
-export default function App() {
+function App() {
   return (
-    <Authenticator>
-    {({ signOut, user }) => (
     <div className="App">
       <header>
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hi {user.username}! We now have Auth!</h1>
+        <h1>We now have Auth!</h1>
       </header>
-      <button onClick={signOut} class="amplify-button" data-variation="primary" type="submit">Sign out</button>
+      <AmplifySignOut />
     </div>
-    )}
-    </Authenticator>
   );
 }
 
+export default withAuthenticator(App);
